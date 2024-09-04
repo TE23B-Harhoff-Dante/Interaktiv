b073aed589b673616b124bafe9af2bb5e66af8f6
Dante Hahroff <dante.harhoff@elev.ga.ntig.se>
2024-09-04 14:50:25 +0200

    rgssfvfs

--- Interaktiv/Program.cs
+++ Interaktiv/Program.cs
@@ -1,3 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+
+
+
+string choice = "";
+string word = "";
+
+Console.WriteLine("Du vaknar och ser två dörrar framför dig.\n");
+Console.WriteLine("Den ena dörren är blå och den andra röd.\n");
+Console.WriteLine("vilken dörr väljer du?");
+
+choice = Console.ReadLine();
+
+
+if (choice == "röd")
+{
+
+    Console.WriteLine("Du ser 3 nya dörrar");
+    Console.WriteLine("\nEn svart, en grön och en lila");
+    Console.WriteLine("\nVilken väljer du");
+
+    word = Console.ReadLine();
+
+
+    if (word == "lila")
+    {
+
+        Console.WriteLine("YAY det är dörren till en miljard kronor");
+    }
+
+    else if (word == "grön")
+    {
+
+        Console.WriteLine(" Det var en dörr till havet (du drunknar)");
+    }
+    else
+    {
+
+        Console.WriteLine(" Det var en dörr till dinosaurietiden (du blir förmodligen upäten)");
+    }
+
+}
+else
+{
+    Console.WriteLine("\nEtt monster var bakom dörren och åt upp dig");
+}
+
+
+
+Console.ReadLine();
